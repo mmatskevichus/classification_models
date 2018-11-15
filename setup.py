f0a6ab54bfd3739d039770ac3cb0977c3511961e
mmatskevichus <44808791+mmatskevichus@users.noreply.github.com>
2018-11-15 19:21:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,5 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="classification_models",
     version="0.0.1",
-    author="Forked from https://github.com/qubvel",
-    description= "Pretrained classification models for Keras",
-    long_description= long_description,
-    long_description_content_type= "text/markdown",
-    url= "https://github.com/qubvel/classification_models",
-    packages= setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    author="Forked from https://github.com/qubvel")
 )
