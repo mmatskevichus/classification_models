73ae9962fcb2ebbccc8e9a3c9bc147f31f22cf94
mmatskevichus <44808791+mmatskevichus@users.noreply.github.com>
2018-11-15 21:06:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,5 @@ from setuptools import setup
 
 setup(
     name='classification_models',
-    version='',
-    packages=['', 'tests', 'classification_models', 'classification_models.resnet', 'classification_models.resnext'],
-    package_dir={'': 'classification_models-master'},
-    url='',
-    license='',
-    author='mmatskevichus',
-    author_email='',
-    description=''
+    packages=['classification_models', 'classification_models.resnet', 'classification_models.resnext']
 )
